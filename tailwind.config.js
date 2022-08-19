82fc7c035df70a679ce3cd3a17e36f005d914a09
Favour <toluwaniokunowo@gmail.com>
2022-08-19 23:26:03 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       Manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 }
