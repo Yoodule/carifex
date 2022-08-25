4412dde13ef26472b3de6b32eee90de372b23cb4
Favour <toluwaniokunowo@gmail.com>
2022-08-25 10:12:10 +0100

    use one stylesheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-       Manrope: ["Manrope", "sans-serif"],
+				sans: ["Manrope", ...defaultTheme.fontFamily.sans]
       },
     },
   },
